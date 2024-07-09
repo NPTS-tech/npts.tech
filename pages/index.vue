7205f30ee18a6e29c0d091480ee542cc3df32ea8
pariah <pariah@pop-os.localdomain>
2024-07-09 00:46:25 -0500

    update partners

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
 main.flex.flex-col.gap-5
   .relative
-    img.filter.blur-sm.mx-auto.max-h-96(src="/img/stock_hero.webp" fit="fill")
+    img.filter.blur-sm.mx-auto(src="/img/stock_hero.webp" fit="fill")
     .absolute.-bottom-4.bg-gradient-to-b.from-transparent.to-black.text-lg.w-full(class="h-full sm:h-3/4")
       .text-center.text-strong.text-lg.mt-20(class="sm:mt-24 sm:text-4xl") Non-Profit Technical Support
       .text-center.text-strong.text-4xs(class="sm:mt-4") Technology for community service
