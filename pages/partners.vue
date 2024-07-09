6cdf1b96d863e4b09342e742ab53e209366702d5
pariah <pariah@pop-os.localdomain>
2024-07-08 23:23:28 -0500

    update partners

--- pages/partners.vue
+++ pages/partners.vue
@@ -3,7 +3,8 @@ Content
   template(#title)
     | Partners
   template(#content)
-    .grid.grid-cols-1.gap-5(class="sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4")
+    //.grid.grid-cols-1.gap-5(class="sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4")
+    .grid.grid-cols-1.gap-5
       .flex.flex-col.gap-5(v-for="partner, i in partners" :key="i" :partner)
         img.h-36.self-center.object-contain(:src="partner.logo")
         .self-center.grow {{partner.name}}
