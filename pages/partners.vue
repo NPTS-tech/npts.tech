86696a653354ffd278e0cbbf880663cdf1658e99
pariah <pariah@pop-os.localdomain>
2024-07-09 00:14:48 -0500

    update partners

--- pages/partners.vue
+++ pages/partners.vue
@@ -1,12 +1,13 @@
 <template lang="pug">
-UCard(class="w-3/4 mx-auto ")
-  template(#header)
-    .text-center.text-4xl Partners
-  //.grid.grid-cols-1.gap-5(class="sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4")
-  .grid.grid-cols-1.gap-5
-    .flex.flex-col.gap-5(v-for="partner, i in partners" :key="i" :partner)
-      img.h-36.self-center.object-contain(:src="partner.logo")
-      .self-center.grow {{partner.name}}
+main.pt-4
+  UCard(class="w-3/4 mx-auto")
+    template(#header)
+      .text-center.text-4xl Partners
+    //.grid.grid-cols-1.gap-5(class="sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4")
+    .grid.grid-cols-1.gap-5
+      .flex.flex-col.gap-5(v-for="partner, i in partners" :key="i" :partner)
+        img.h-36.self-center.object-contain(:src="partner.logo")
+        .self-center.grow {{partner.name}}
 </template>
 <script setup lang="ts">
 const partners = [
