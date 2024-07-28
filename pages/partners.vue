09fcb3a4c599e70868bf0f4b9ab68f5e6d6f643e
MStolen <max@amaxsteele.com>
2024-07-28 02:44:44 -0500

    Fix to partner links

--- pages/partners.vue
+++ pages/partners.vue
@@ -6,11 +6,9 @@ main.pt-4
     //.grid.grid-cols-1.gap-5(class="sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4")
     .grid.grid-cols-2.gap-5
       .flex.flex-col.gap-5(v-for="partner, i in partners" :key="i" :partner)
-        //- I think it would be ideal to make the image and text all in one tag
-        a.self-center.object-contain(:href="partner.website", target="_blank")
+        a.self-center(:href="partner.website", target="_blank")
           img.h-36.self-center.object-contain(:src="partner.logo")
-        a.self-center.object-contain(:href="partner.website", target="_blank")
-          .self-center.grow {{partner.name}}
+          .self-center.grow.text-center {{ partner.name }}
 </template>
 <script setup lang="ts">
 definePageMeta({ documentDriven: { page: false, } });
